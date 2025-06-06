e44fe788590672f0d12b1e52e980f67bd33019b1
klay.yuan <klay.yuan@anker-in.com>
2025-06-06 10:53:52 +0800

    fix: @Klay 删除.next文件

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/anker/Desktop/1/personal_website/app/layout\":[\"static/media/569ce4b8f30dc480-s.p.woff2\",\"static/media/93f479601ee12b01-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
